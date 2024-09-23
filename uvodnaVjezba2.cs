47bc358702ddd51edd3da35b6a1eef905411ebc5
SeadColakovic <flegarsead263@gmail.com>
2024-09-23 19:23:04 +0200

    Add files via upload

---
+++ uvodnaVjezba2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int main()
+            {
+                int a = 10, b = 7;
+                double c = 12.5, d = 16.7;
+
+                double prosjek = (a + b + c + d) / 4.0;
+                Console.WriteLine("Prosječna vrijednost je: {0}", prosjek);
+                return 0;
+            }
+
+
+
+
+
+
+        }
+    }
+}
